255a4dd8eefcf5ecf85f3aed953c2a9a6e8d03cd
hugo valois <hvgaming@yahoo.com>
2018-10-23 01:27:19 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,24 @@
 require 'pry'
 
+class Song
 
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
+  attr_accessor :name
+  attr_reader :artist
+
+  @@songs = []
+
+  def self.all
+    @@songs
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self) unless artist.songs.include?(self)
+  end
+  
+end
